e78215072eff15eada5b218119723304107ad99e
Gowtham Punniyaseelan <88957910+gowthampunniyaseelan@users.noreply.github.com>
2024-08-27 20:22:54 +0530

    added catch block

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,23 +3,20 @@ import axios from 'axios';
 const API_URL = 'https://emayam-todo-backend.onrender.com/api/auth';
 
 export const login = (email, password) => {
-  try{
-    const res = axios.post(`${API_URL}/login`, { email, password });
-    alert("Loggin successfully")
+    axios.post(`${API_URL}/login`, { email, password }).then((res)=>{
+    alert("Login successfully")
     return res
-  }catch(err){
-    alert("Check your email or password")
-  }
-  
+    }).catch((err)=>{
+      alert("Check your email or password")
+    })
 };
 
 export const register = (username, email, password,role) => {
-  try{
-    const res = axios.post(`${API_URL}/register`, { username, email, password , role});
-    alert("Registered successfully")
-    return res
-  }catch(err){
-    alert("Email already exist please login")
-  }
-  
+    axios.post(`${API_URL}/register`, { username, email, password , role}).then((res)=>{
+      alert("Registered successfully")
+      return res
+    }).catch((err)=>{
+      alert("Email already exist please login")
+    })
+   
 };
