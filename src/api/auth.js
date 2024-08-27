a8446b4cba8ea81d6628f6f418c5550f5d041241
Gowtham Punniyaseelan <88957910+gowthampunniyaseelan@users.noreply.github.com>
2024-08-27 20:26:15 +0530

    added try catch

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,21 +2,23 @@ import axios from 'axios';
 
 const API_URL = 'https://emayam-todo-backend.onrender.com/api/auth';
 
-export const login = (email, password) => {
-    axios.post(`${API_URL}/login`, { email, password }).then((res)=>{
-    alert("Login successfully")
-    return res
-    }).catch((err)=>{
-      alert("Check your email or password")
-    })
+export const login = async (email, password) => {
+    try {
+    const res = await axios.post(`${API_URL}/login`, { email, password });
+    alert("Login successfully");
+    return res;
+  } catch (err) {
+    alert("Something wrong Check your email or password");
+  }
 };
 
-export const register = (username, email, password,role) => {
-    axios.post(`${API_URL}/register`, { username, email, password , role}).then((res)=>{
-      alert("Registered successfully")
-      return res
-    }).catch((err)=>{
-      alert("Email already exist please login")
-    })
+export const register = async (username, email, password,role) => {
+    try {
+    const res = await axios.post(`${API_URL}/register`, { username, email, password, role });
+    alert("Registered successfully");
+    return res;
+  } catch (err) {
+    alert("Email already exist please login");
+  }
    
 };
